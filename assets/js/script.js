d4ef1945adc8b9f56771463a192bb7fc6bffb9d0
medinat akinsowon <medinahakinsowon@gmail.com>
2024-01-16 12:18:56 +0100

    media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,10 @@ const togglerNavbar = function () {
 addEventsOnElements(navTogglers, 'click', togglerNavbar)
 
 
-/*--Header---*/
+/*--Header-- &--BackToTop-*/
 
 const header = document.querySelector('[data-header]')
+const backTopBtn = document.querySelector('[data-back-top-btn]')
 
 let lastScrollPos = 0;
 
@@ -58,9 +59,11 @@ const hideHeader = function (){
 window.addEventListener('scroll', function () {
   if (window.scrollY >= 50){
     header.classList.add('active')
+    backTopBtn.classList.add('active')
     hideHeader();
   } else {
     header.classList.remove('active')
+    backTopBtn.classList.remove('active')
   }
 });
 
